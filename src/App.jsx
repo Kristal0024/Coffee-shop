f2a1a9e5c456a1eb3393675d8600c650405ff204
Kristal Shrestha <kristalstha20@gmail.com>
2026-04-27 23:06:51 +0545

    new story section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import NavBar from './pages/NavBar'
 import Home from './pages/Home'
 import Featured from './pages/Featured'
+import Story from './pages/Story'
 function App() {
   return (
     <>
@@ -10,6 +11,7 @@ function App() {
         <NavBar/>
           <Home/>
           <Featured/>
+          <Story/>
       </div>
     </>
   )
---
+++ src/pages/Story.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Baristaimg from "../assets/Barista.png";
+
+const Story = () => {
+  return (
+    <div>
+      <div className="p-20 w-full h-screen bg-secondary flex">
+        <div className="flex w-full items-center justify-center">
+          <div className="w-[45%] mr-11">
+            <img className="w-full rounded-2xl" src={Baristaimg} alt="Barista working image" />
+          </div>
+          <div className="w-[50%]">
+            <h1 className="text-4xl mb-6">More Than Just Coffee</h1>
+            <p className="text-lg mb-8 text-p">
+              At Aura Cafe, we believe in the power of a pause. Our space is
+              designed to be your third place—a warm, inviting environment where
+              the aroma of fresh espresso meets the comfort of home. Whether
+              you're catching up with friends or finding a quiet corner to work,
+              you belong here.
+            </p>
+            <button className="text-button border-b-2">Read Our Story</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Story;
